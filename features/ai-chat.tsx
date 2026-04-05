9a5bf8d77bc5fca6601460e1174660a0f38734dc
KwaHash <cosmosray001@gmail.com>
2026-04-05 18:09:39 +0900

    feat: main pages

---
+++ features/ai-chat.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Thread } from '@/components/thread'
+import { VoteRuntimeProvider } from '@/providers/vote-runtime-provider'
+
+const AIChatPage = () => {
+  return (
+    <VoteRuntimeProvider>
+      <div className='flex w-full h-[calc(100dvh_-_88px)] pr-0.5 overflow-hidden'>
+        <div className='absolute inset-0 opacity-[0.04]'
+            style={{
+              backgroundImage: 'linear-gradient(to right, #1f2937 1px, transparent 1px), linear-gradient(to bottom, #1f2937 1px, transparent 1px)',
+              backgroundSize: '35px 35px'
+            }}
+          />
+        <div className='flex-1 overflow-hidden'>
+          <Thread />
+        </div>
+      </div>
+    </VoteRuntimeProvider>
+  )
+}
+
+export default AIChatPage
