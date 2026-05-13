bed99b3f2a205898bf1ffcc5b830edbfcfc17d80
KwaHash <cosmosray001@gmail.com>
2026-05-13 17:51:57 +0900

    feat: user_name in auth-provider

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -56,6 +56,7 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({
       user_id: payload.user_id,
       user_email: payload.user_email,
+      user_name: user.username != null ? String(user.username) : '',
       user_role: user.role,
       is_authenticated: true,
       access_token: validAccessToken,
